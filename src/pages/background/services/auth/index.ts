c7e00af856a9163c90d6087fb2b7b04c77b1b990
Manish Mandal <manishmandalj@gmail.com>
2023-10-07 19:27:51 +0530

    feat: new auth flow: successfuly tested new auth system

--- src/pages/background/services/auth/index.ts
+++ src/pages/background/services/auth/index.ts
@@ -9,49 +9,58 @@ type GetAutTokenResponse = {
 };
 
 // launches google auth flow for user to grant permission to their gmail
-const launchGoogleAuthFlow = async (userId: string): Promise<GetAutTokenResponse> => {
+const launchGoogleAuthFlow = async (email: string): Promise<GetAutTokenResponse> => {
   let clientId = '145716100092-ivrvur7d0s0bugdgq4ueinf646q8ih7b.apps.googleusercontent.com';
   let redirectUri = `https://${chrome.runtime.id}.chromiumapp.org/`;
-  let nonce = Math.random().toString(36).substring(2, 15);
+  // let nonce = Math.random().toString(36).substring(2, 15);
+
+  const scope = `openid email https://mail.google.com https://www.googleapis.com/auth/gmail.settings.basic`;
   try {
+    // TODO:
+    // change response type to code (get access & refresh tokens)
+
+    // send email as intent
+
+    // try to set the maximum age for the token
+
+    // store these token with expiry time (email, access token,  refresh token, expiry time)
+
+    // this store will define if the user is logged in or not
+
+    // we can check the token validity each time we make a call to google api
+
+    //
+
     const authUrl = new URL('https://accounts.google.com/o/oauth2/v2/auth');
 
     authUrl.searchParams.set('client_id', clientId);
-    authUrl.searchParams.set('response_type', 'id_token');
     authUrl.searchParams.set('redirect_uri', redirectUri);
+    // login type
+    // authUrl.searchParams.set('access_type', 'offline');
+    authUrl.searchParams.set('response_type', 'token');
     // Add the OpenID scope. Scopes allow you to access the user’s information.
-    authUrl.searchParams.set('scope', 'openid profile email');
-    authUrl.searchParams.set('nonce', nonce);
-    // Show the consent screen after login.
-    authUrl.searchParams.set('prompt', 'consent');
+    authUrl.searchParams.set('scope', scope);
+    // authUrl.searchParams.set('nonce', nonce);
+    authUrl.searchParams.set('login_hint', 'mmjdd67@gmail.com');
+    // consent prompt, (none shows no consent screen)
+    // authUrl.searchParams.set('prompt', 'none');
 
     //
 
     chrome.identity.launchWebAuthFlow(
       {
         url: authUrl.href,
-        interactive: true,
+        interactive: false,
       },
       redirectUrl => {
         if (redirectUrl) {
-          // The ID token is in the URL hash
-          const urlHash = redirectUrl.split('#')[1];
-          const params = new URLSearchParams(urlHash);
-          const jwt = params.get('id_token');
+          console.log('🚀 ~ file: index.ts:56 ~ launchGoogleAuthFlow ~ redirectUrl:', redirectUrl);
 
-          console.log('🔥 ~ file: index.ts:42 ~ launchGoogleAuthFlow ~ jwt:', jwt);
+          const token = redirectUrl.split('#')[1]?.split('=')[1];
 
-          // Parse the JSON Web Token
-          const base64Url = jwt.split('.')[1];
-          const base64 = base64Url.replace('-', '+').replace('_', '/');
-          const parsedJWT = JSON.parse(atob(base64));
+          if (!token) throw new Error('Token not found.');
 
-          //
-          const email = parsedJWT.email;
-          const name = parsedJWT.given_name;
-          const token = jwt;
-
-          console.log('🔥 🔥 ~ file: index.ts:49 ~ launchGoogleAuthFlow ~ parsedJWT:', parsedJWT);
+          return token;
         }
       }
     );
@@ -101,8 +110,8 @@ const getUserInfo = (): Promise<IUserInfo | null> => {
 const clearToken = async (token: string) => {
   // return new Promise<boolean>((resolve, reject) => {
   try {
-    const url = 'https://accounts.google.com/o/oauth2/revoke?token=' + token;
-    await fetch(url);
+    // const url = 'https://accounts.google.com/o/oauth2/revoke?token=' + token;
+    // await fetch(url);
 
     await chrome.identity.clearAllCachedAuthTokens();
 
